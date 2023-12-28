3648210002f17c07f09840b91b2116c44f3e9731
Rhonda Sheppard <rhondasheppard32@gmail.com>
2023-12-28 17:10:28 -0500

    Router Setup

--- NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
@@ -4,7 +4,6 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-    
+export const selectCampsitesById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
--- NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
+++ NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
@@ -1,20 +1,19 @@
 import { useState } from 'react'; 
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsitesById } from '../features/campsites/campsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-   const [selectedCampsite, toggleCampsite] = useState(selectRandomCampsite());
+   const [campsiteId, setCampsiteId] = useState(0);
+   const selectedCampsite = selectCampsitesById(campsiteId);
 
     return (
         <Container>
-            <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
-                Select Random Campsite
-            </Button>
+           
             <Row>
                 <Col sm='5' md='7'>
-                    <CampsitesList />
+                    <CampsitesList setCampsiteId={setCampsiteId} />
                 </Col>
                 <Col sm='7' md='5'>
                     <CampsiteDetail campsite={selectedCampsite} />
