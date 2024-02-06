42ec0891315e51354d04b780ac4e63c8636f53f1
Rhonda Sheppard <rhondasheppard32@gmail.com>
2024-02-06 17:40:05 -0500

    Setting up Redux

--- NucampFolder/WebDev/4-React-Native/nucampsite/screens/MainComponent.js
+++ NucampFolder/WebDev/4-React-Native/nucampsite/screens/MainComponent.js
@@ -1,14 +1,19 @@
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View, Text, Image } from "react-native";
 import Constants from "expo-constants";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
 import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import { Icon } from "react-native-elements";
 import { color } from "react-native-reanimated";
+import logo from "../assets/images/logo.png";
 
 const Drawer = createDrawerNavigator();
 
@@ -115,6 +120,20 @@ const DirectoryNavigator = () => {
   );
 };
 
+const CustomDrawerContent = (props) => (
+  <DrawerContentScrollView {...props}>
+    <View style={styles.drawerHeader}>
+      <View style={{ flex: 1 }}>
+         <Image source={logo} style={styles.drawerImage} />
+      </View>
+      <View style={{ flex: 2 }}>
+         <Text style={styles.drawerHeaderText}>NuCamp</Text>
+      </View>
+    </View>
+    <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold' }} />
+  </DrawerContentScrollView>
+);
+
 const Main = () => {
   return (
     <View
@@ -126,6 +145,7 @@ const Main = () => {
       <Drawer.Navigator
         initialRouteName="Home"
         drawerStyle={{ backgroundColor: "#CEC8FF" }}
+        drawerContent={ CustomDrawerContent }
       >
         <Drawer.Screen
           name="Home"
@@ -140,7 +160,7 @@ const Main = () => {
                 iconStyle={{ width: 24 }}
                 color={color}
               />
-            )
+            ),
           }}
         />
 
@@ -205,6 +225,24 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 24,
   },
+  drawerHeader: {
+    backgroundColor: '#5637DD',
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row'
+},
+drawerHeaderText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+},
+drawerImage: {
+    margin: 10,
+    height: 60,
+    width: 60
+}
 });
 
 export default Main;
---
+++ NucampFolder/WebDev/4-React-Native/nucampsite/shared/baseURL.js
@@ -0,0 +1,3 @@
+
+
+export const baseUrl = 'http://10.0.0.100:3001/';
